1e7b445ff92af4a6e48f18bb3057d506e1a3d9af
maguimarijuan <magali.marijuan@fizzmod.com>
2020-11-23 15:32:16 -0300

    add response to exercise-2

--- src/exercise2.clj
+++ src/exercise2.clj
@@ -1,11 +1,18 @@
 (ns exercise2)
 
+(defn only-greater-than-five-recursive
+  "Recursive wrapper to main function"
+  [x i aux]
+  (if (= i -1)
+    aux
+    (if(> (nth x i) 5)
+        (only-greater-than-five-recursive x (dec i) (conj aux (nth x i)))
+        (only-greater-than-five-recursive x (dec i) aux)))
+)
+
 (defn only-greater-than-five
   "Filters the input list returning only the elements greater than 5."
   [x]
-  (throw (Exception. "Not implemented."))
+  (only-greater-than-five-recursive x (dec (count x)) ())
 )
-
-
-
 
